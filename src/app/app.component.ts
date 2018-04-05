fdb3deaead93a4f91c011be2462500cb5dbbb291
Mingyu Gao <mingyu.gao96@gmail.com>
2018-04-05 10:20:10 -0700

    update status bar color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleLightContent();
-      this.statusBar.backgroundColorByHexString('#00227b');
+      this.statusBar.backgroundColorByHexString('#398964');
       this.splashScreen.hide();
     });
   }
